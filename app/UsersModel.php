cb94652c1f1c695a4da650865e019b8f8c687fe5
hadadalgojali <hadadalgojali@gmail.com>
2019-05-29 15:59:46 +0700

    Setup Model dan koneksi

---
+++ app/UsersModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UsersModel extends Model
+{
+    //
+    protected $table = 'users';
+    protected $fillable = ['id', 'first_name', 'last_name', 'phone', 'address', 'email'];
+}
---
+++ database/migrations/2019_05_29_085104_create_users_models_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('id');
+            $table->primary('id');
+            $table->string('first_name', 50);
+            $table->string('last_name', 120);
+            $table->string('phone', 15);
+            $table->string('email', 100);
+            $table->unique('email');
+            $table->text('address');
+            $table->datetime('updated_at');
+            $table->datetime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_models');
+    }
+}
