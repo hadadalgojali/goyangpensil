163180b8bcc5b08f149d88ace7ac1890cd98aa9c
Hadad Al Gojali <hadadalgojali@gmail.com>
2019-08-20 23:21:24 +0700

    Page Contact

--- resources/views/pages/contact/index.blade.php
+++ resources/views/pages/contact/index.blade.php
@@ -1,4 +1,20 @@
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<style type="text/css">
+  #load_screen{
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('//upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Phi_fenomeni.gif/50px-Phi_fenomeni.gif')
+                50% 50% no-repeat rgb(249,249,249);
+  }
+</style>
+
+<div id="load_screen"></div>
 @extends('index', ['title' => 'GP - Kontak'])
+@section('content')
 <!-- <div id="content" style="display:none;"> -->
 <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url({{URL::to('/')}}/template/public/images/banner-min.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
   <div class="container">
@@ -85,4 +101,5 @@
         </div>
       </div>
     </div>
+@endsection
 <!-- </div> -->
